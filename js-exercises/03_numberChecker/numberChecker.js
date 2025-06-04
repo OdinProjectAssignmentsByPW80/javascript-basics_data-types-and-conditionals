9eafe26a71e5d34987f7f1ac27f3e6b0df92a745
Paul Williams <advanced_solution_delivery@outlook.com>
2025-06-04 05:58:35 +0100

    feat: 03_numberChecker
    Changes to be committed:
    	modified:   js-exercises/03_numberChecker/numberChecker.js
    	modified:   js-exercises/03_numberChecker/numberChecker.spec.js

--- js-exercises/03_numberChecker/numberChecker.js
+++ js-exercises/03_numberChecker/numberChecker.js
@@ -1,10 +1,16 @@
 function numberChecker(number) {
-  if (number === 6) {
-    return true;
-  } else {
-    return false;
-  }
+  return number >= 10;
 }
 
 // Do not edit below this line
 module.exports = numberChecker;
+
+/*
+  Solution:
+  if (number >= 10) {
+    return true;
+  } else {
+    return false;
+  }
+  Was fairly confident this would be the case and decided I didn't care.
+*/
